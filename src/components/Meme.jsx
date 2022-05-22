9c51657670435db68b1908720a9dddc3f6078200
Chijioke Chika <8pluscreative@gmail.com>
2022-05-22 18:43:05 -0400

    Meme Rendering

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './Meme.css'
 import MemesData from "./MemesData";
 
 const Meme = () => {
-	let url;
+	const [memeImage, setMemeImage] = useState('');
 
 	const getMemeImage = (props) => {
 		const memesArr = MemesData.data.memes;
-  	const randomNum = Math.floor(Math.random() * memesArr.length);
-  	url = memesArr[randomNum].url
-    console.log(url)
+		const randomNum = Math.floor(Math.random() * memesArr.length);
+		setMemeImage(memesArr[randomNum].url) 
 	}
 
 	return (
 		<div>
 			<div action="submit" className="form">
-					<input type="text" className="form-input" placeholder="Top text"/>
+					<input type="text" className="form-input" placeholder="Top Text"/>
 					<input type="text" className="form-input" placeholder="Bottom Text"/>
 					<button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+					<div className="image-container">
+						<img src={memeImage} alt="" />
+					</div>
 			</div>
 		</div>
 	)
