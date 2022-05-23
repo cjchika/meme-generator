dbe9d65b4ef3328c35a99be78373992aa4291537
Chijioke Chika <8pluscreative@gmail.com>
2022-05-23 13:19:41 -0400

    Refactor

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,12 +3,17 @@ import './Meme.css'
 import MemesData from "./MemesData";
 
 const Meme = () => {
-	const [memeImage, setMemeImage] = useState('');
+	const [meme, setMeme] = useState({topText: "", bottomText: "", randomImage: ""});
+	const [allMemeImages, setAllMemeImages] = useState(MemesData)
 
 	const getMemeImage = (props) => {
-		const memesArr = MemesData.data.memes;
+		const memesArr = allMemeImages.data.memes;
 		const randomNum = Math.floor(Math.random() * memesArr.length);
-		setMemeImage(memesArr[randomNum].url) 
+		const url = memesArr[randomNum].url
+		setMeme(prevMeme => ({
+			...prevMeme, 
+			randomImage: url
+		}))
 	}
 
 	return (
@@ -18,7 +23,7 @@ const Meme = () => {
 					<input type="text" className="form-input" placeholder="Bottom Text"/>
 					<button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
 					<div className="image-container">
-						<img src={memeImage} alt="" />
+						<img src={meme.randomImage} alt="" />
 					</div>
 			</div>
 		</div>
