3ea292806f9d2743a71d0cc0393408b9d68ddd07
Chijioke Chika <8pluscreative@gmail.com>
2022-05-26 02:59:18 -0400

    Getting Memes from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Meme from "./components/Meme";
 import Navbar from "./components/Navbar";
-import MemesData from "./components/MemesData";
 
 function App() {
-  const memeArr = MemesData.data.memes;
-  const randomNum = Math.floor(Math.random() * memeArr.length);
-  console.log(randomNum);
+  
 
   return (
     <div className="App">
